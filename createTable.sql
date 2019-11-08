568db6185702eba73bad1668f49f75511225b67b
playtoc <nevo@naver.com>
2019-11-08 10:22:49 +0900

    createTable

---
+++ createTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `table` (
+  uid int(9) unsigned NOT NULL auto_increment,
+  name varchar(20) NOT NULL,
+  subject varchar(30) NOT NULL default '',
+  date date,
+  time time,
+  datetime datetime,
+  content text,
+  age int(2) default 0,
+  PRIMARY KEY (uid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
