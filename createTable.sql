af6e3d25e4c2823ab06921df6faf57007dcce260
playtoc <nevo@naver.com>
2021-04-16 18:33:42 +0900

    add password

--- createTable.sql
+++ createTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `table` (
   uid int(9) unsigned NOT NULL auto_increment,
   name varchar(20) NOT NULL,
+  password varchar(255) NOT NULL,
   subject varchar(30) NOT NULL default '',
   date date,
   time time,
